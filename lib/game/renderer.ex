f1834db68a7c55ea1fda70e96d1e656076e57797
nmandrelas <nmandrelas@gmail.com>
2026-01-01 11:32:30 +0200

    initial

--- lib/game/renderer.ex
+++ lib/game/renderer.ex
@@ -11,7 +11,7 @@ defmodule Game.Renderer do
     for y <- 0..(@height - 1) do
       for x <- 0..(@width - 1) do
         if npc_at?(world.npcs, x, y) do
-          get_symbol(world.npcs, x, y)
+          "@"
         else
           " "
         end
@@ -23,21 +23,5 @@ defmodule Game.Renderer do
 
   defp npc_at?(npcs, x, y) do
     Enum.any?(npcs, fn %NPC{x: nx, y: ny} -> nx == x && ny == y end)
-  end
-
-  defp get_symbol(npcs, x, y) do
-    case Enum.find(npcs, fn %NPC{x: nx, y: ny} -> nx == x && ny == y end) do
-      nil -> "."
-      %NPC{race: race} -> symbol_for_race(race)
-    end
-  end
-
-  defp symbol_for_race(race) do
-    case race do
-      "human" -> "H"
-      "goblin" -> "g"
-      "orc" -> "O"
-      _ -> "?"
-    end
   end
 end
