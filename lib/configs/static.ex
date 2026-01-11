f0c8b3679f08e568cadf6e54a279fd61d650f555
nmandrelas <nmandrelas@gmail.com>
2026-01-11 22:11:32 +0200

    World map added

--- lib/configs/static.ex
+++ lib/configs/static.ex
@@ -1,4 +1,7 @@
 defmodule Configs.Static do
   def width(), do: 16
   def height(), do: 16
+  def world_map_height(), do: 10
+  def world_map_width(), do: 25
 end
+
---
+++ lib/game/world_map.ex
@@ -0,0 +1,17 @@
+defmodule Game.WorldMap do
+  alias Configs.Static
+
+  defstruct cells: %{}
+
+  def new do
+    cells =
+      for x <- 0..(Static.world_map_width() - 1),
+          y <- 0..(Static.world_map_height() - 1),
+          into: %{} do
+        {{x, y}, {Game.World.new()}}
+      end
+
+    %Game.WorldMap{cells: cells}
+  end
+end
+
